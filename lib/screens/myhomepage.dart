4970d0005cdf40ba2b80e924b8de40157061f89f
Luciano Silveira de Souza Filho <filholuciano392@gmail.com>
2025-11-12 11:48:23 -0300

    Add files via upload

---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,63 @@
+import 'package:aula6b/model/profile.dart';
+import 'package:aula6b/widgets/mycard.dart';
+import 'package:aula6b/widgets/mydrawer.dart';
+import 'package:aula6b/widgets/mystory.dart';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+    //Criando lista de dados
+  List<Profile> listaDados = [
+    Profile(name: "MrBeast", image:"https://i.scdn.co/image/ab67616d00001e02b21a44be65b9cf61d2e5e9a6", like: true),
+    Profile(name: "Giovana", image: "", like: false),
+    Profile(name: "Hilário", image: "", like: true),
+    Profile(name: "Homer", image: "", like: false),
+    Profile(name: "Augusta", image: "", like: true)
+  ];
+MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aula 6"),
+        backgroundColor: Colors.greenAccent,
+      ),
+      drawer: MyDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount:listaDados.length,
+                itemBuilder: (context, item) {
+                  return MyStory(title:
+                  listaDados[item].name,
+                  image: listaDados[item].image,
+                  );
+                }
+              ),
+            ),
+
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: listaDados.length,
+                itemBuilder: (context, item) {
+                  return MyCard(
+                    title: listaDados[item].name,
+                     image: listaDados[item].image,
+                      like: listaDados[item].like
+                      );
+                }
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
